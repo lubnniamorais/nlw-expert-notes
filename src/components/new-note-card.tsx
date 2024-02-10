f590f55f02fd27f642e75d86c12792250aaa6462
Lubnnia <lubnnia@gmail.com>
2024-02-10 18:21:33 -0300

    feat: Add dialog component of radix-ui

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,10 +1,19 @@
-export function NewNoteCard() {
+import * as Dialog from '@radix-ui/react-dialog';
+
+const NewNoteCard = () => {
   return (
-    <div className="rounded-md bg-slate-700 p-5 space-y-3">
-      <span className="text-sm font-medium text-slate-200">Adicionar nota</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Grave uma nota em áudio que será convertida para texto automaticamente.
-      </p>
-    </div>
+    <Dialog.Root>
+      <div className="rounded-md bg-slate-700 p-5 space-y-3">
+        <span className="text-sm font-medium text-slate-200">
+          Adicionar nota
+        </span>
+        <p className="text-sm leading-6 text-slate-400">
+          Grave uma nota em áudio que será convertida para texto
+          automaticamente.
+        </p>
+      </div>
+    </Dialog.Root>
   );
-}
+};
+
+export { NewNoteCard };
